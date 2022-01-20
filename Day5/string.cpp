b5b4b31119fdb38b852487fd0fbbb39d10b1b58d
Aelaz24 <andyaziz24@gmail.com>
2022-01-20 12:58:37 -0500

    string class

---
+++ Day5/string.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string greeting = "Hello There!";
+    
+
+    //getting string input
+
+    std::string name, email;
+    std::cout << "Your name ? ";
+    getline(std::cin, name);
+    std::cout << "Your email ? ";
+    getline(std::cin, email);
+    std::cout << name << std::endl << email;
+    
+
+
+    //string modifier
+
+
+
+    //string contstan/literal
+
+    auto a = 30; // decimal
+    auto b = 02; // octal
+    auto c = 0x4b; // hexadecimal
+
+
+
+
+
+
+    
+}
